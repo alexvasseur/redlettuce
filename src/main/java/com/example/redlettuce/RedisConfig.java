e6bed332c41ae912d2444b1b1d5392a911f52c47
evanTheTerribleWarrior <vresv86@gmail.com>
2025-06-04 09:57:26 +0100

    [ER] added extra application.properties, removed hardcoded values

--- src/main/java/com/example/redlettuce/RedisConfig.java
+++ src/main/java/com/example/redlettuce/RedisConfig.java
@@ -4,6 +4,7 @@ import io.lettuce.core.ClientOptions;
 import io.lettuce.core.resource.ClientResources;
 import io.lettuce.core.resource.Delay;
 import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
@@ -14,22 +15,34 @@ import java.time.Duration;
 @Configuration
 public class RedisConfig {
 
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.redis.commandTimeout}")
+    private int commandTimeout;
+
+    @Value("${spring.redis.reconnectDelay}")
+    private int reconnectDelay;
+
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
         LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
-            .commandTimeout(Duration.ofSeconds(3)) //TODO this overrides the application.properties
+            .commandTimeout(Duration.ofSeconds(commandTimeout))
             .shutdownTimeout(Duration.ZERO)
             .clientOptions(ClientOptions.builder()
                 .autoReconnect(true)
                 .build())
             .clientResources(ClientResources.builder()
-                .reconnectDelay(Delay.constant(Duration.ofMillis(200))) //TODO this could be configurable
+                .reconnectDelay(Delay.constant(Duration.ofMillis(reconnectDelay))) 
                 .build())
             .build();
 
         RedisStandaloneConfiguration serverConfig = new RedisStandaloneConfiguration();
-        serverConfig.setHostName("localhost"); //TODO this overrides the application.properties
-        serverConfig.setPort(6379); //TODO this overrides the application.properties
+        serverConfig.setHostName(redisHost);
+        serverConfig.setPort(redisPort); 
         return new LettuceConnectionFactory(serverConfig, clientConfig);
     }
 }
