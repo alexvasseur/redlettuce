e249b0326e5b540a4d60acf04e6234fee00f1694
evanTheTerribleWarrior <vresv86@gmail.com>
2025-06-04 10:29:17 +0100

    [ER] added password

--- src/main/java/com/example/redlettuce/RedisConfig.java
+++ src/main/java/com/example/redlettuce/RedisConfig.java
@@ -6,6 +6,7 @@ import io.lettuce.core.resource.Delay;
 import org.springframework.context.annotation.Bean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -15,34 +16,42 @@ import java.time.Duration;
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.redis.host}")
+    @Value("${spring.data.redis.host}")
     private String redisHost;
 
-    @Value("${spring.redis.port}")
+    @Value("${spring.data.redis.port}")
     private int redisPort;
 
-    @Value("${spring.redis.commandTimeout}")
-    private int commandTimeout;
+    @Value("${spring.data.redis.password}")
+    private String redisPassword;
 
-    @Value("${spring.redis.reconnectDelay}")
-    private int reconnectDelay;
+    @Value("${spring.data.redis.command-timeout}")
+    private Duration commandTimeout;
+
+    @Value("${spring.data.redis.reconnect-delay}")
+    private Duration reconnectDelay;
 
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
         LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
-            .commandTimeout(Duration.ofSeconds(commandTimeout))
+            .commandTimeout(commandTimeout)
             .shutdownTimeout(Duration.ZERO)
             .clientOptions(ClientOptions.builder()
                 .autoReconnect(true)
                 .build())
             .clientResources(ClientResources.builder()
-                .reconnectDelay(Delay.constant(Duration.ofMillis(reconnectDelay))) 
+                .reconnectDelay(Delay.constant(reconnectDelay)) 
                 .build())
             .build();
 
         RedisStandaloneConfiguration serverConfig = new RedisStandaloneConfiguration();
         serverConfig.setHostName(redisHost);
         serverConfig.setPort(redisPort); 
+        
+        if (redisPassword != null && !redisPassword.isBlank()) {
+            serverConfig.setPassword(RedisPassword.of(redisPassword));
+        } 
+
         return new LettuceConnectionFactory(serverConfig, clientConfig);
     }
 }
